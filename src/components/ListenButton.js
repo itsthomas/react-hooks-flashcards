c7e7f72910282d81972bc825f6cc60ae4944cfa1
Kayut <kayut@192.168.1.124>
2021-01-12 23:19:25 +0000

    clean up

--- src/components/ListenButton.js
+++ src/components/ListenButton.js
@@ -9,13 +9,13 @@ const ListenButton = (props) => {
   return (
     <>
     <label onClick={(e) => {
-                  if(message.length > 0) {
-                    const msg = new SpeechSynthesisUtterance(message);
-                    msg.lang = language;
-                    msg.rate = rate;
-                    msg.pitch = pitch;
-                    window.speechSynthesis.speak(msg);
-                  }
+      if(message.length > 0) {
+        const msg = new SpeechSynthesisUtterance(message);
+        msg.lang = language;
+        msg.rate = rate;
+        msg.pitch = pitch;
+        window.speechSynthesis.speak(msg);
+      }
 
     e.stopPropagation();
   }
