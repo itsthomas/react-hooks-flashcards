510079888067307f92e0e21f20ba8cc1ca3ec942
Kayut <kayut@192.168.1.124>
2021-01-13 09:59:53 +0000

    Removed helpers folder

--- src/helpers/timeOutScroll.js
+++
@@ -1,8 +0,0 @@
-const timeOutScroll = () => {
-  setTimeout(() => {
-    window.location.reload();
-    window.scrollTo(0, document.querySelector('.App').scrollHeight);
-  }, 1000);
-};
-
-export default timeOutScroll;
